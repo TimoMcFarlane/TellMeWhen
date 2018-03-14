0aae352e40d76ef62f4408feba9f2f89c3fe47c9
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2018-03-14 14:53:25 +0200

    Implement RecycleAdapter functionality.

--- app/src/main/java/fi/timomcfarlane/tellmewhen/RecyclerAdapter.java
+++ app/src/main/java/fi/timomcfarlane/tellmewhen/RecyclerAdapter.java
@@ -1,8 +1,54 @@
 package fi.timomcfarlane.tellmewhen;
 
-/**
- * Created by tmcf on 14/03/2018.
- */
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
 
-public class RecyclerAdapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+
+    private ArrayList<Appointment> apps;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public CardView card;
+        public TextView card_title;
+        public TextView card_address;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            card = (CardView)itemView.findViewById(R.id.card_view);
+            card_title = (TextView)itemView.findViewById(R.id.card_text_view_title);
+            card_address = (TextView)itemView.findViewById(R.id.card_text_view_address);
+        }
+    }
+
+    public RecyclerAdapter(ArrayList<Appointment> set) {
+        this.apps = set;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        CardView v = (CardView) LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.list_card_item, parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder holder, int position) {
+        Appointment[] temp = new Appointment[apps.size()];
+        temp = apps.toArray(temp);
+        holder.card_title.setText(temp[position].getTitle());
+        holder.card_address.setText(temp[position].getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return apps.size();
+    }
 }
--- app/src/main/java/fi/timomcfarlane/tellmewhen/ScheduleActivity.java
+++ app/src/main/java/fi/timomcfarlane/tellmewhen/ScheduleActivity.java
@@ -2,12 +2,28 @@ package fi.timomcfarlane.tellmewhen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class ScheduleActivity extends AppCompatActivity {
+    private RecyclerView recycledList;
+    private ArrayList<Appointment> apps;
+    private AppointmentHandler appHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        appHandler = new AppointmentHandler(this);
+        apps = appHandler.getAppointments();
+
+        recycledList = (RecyclerView) findViewById(R.id.recycle_list);
+        recycledList.setLayoutManager(new LinearLayoutManager(this));
+        recycledList.setAdapter(new RecyclerAdapter(apps));
     }
 }
