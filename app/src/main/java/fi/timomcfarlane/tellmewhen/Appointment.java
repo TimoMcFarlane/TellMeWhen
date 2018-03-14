d0973edf5ca00fc5155d9398ed1e78635a580173
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2018-03-14 14:46:11 +0200

    Implement Appointment class.
    Appointment class represents the data object that will be used to display appointments.

--- app/src/main/java/fi/timomcfarlane/tellmewhen/Appointment.java
+++ app/src/main/java/fi/timomcfarlane/tellmewhen/Appointment.java
@@ -1,8 +1,74 @@
 package fi.timomcfarlane.tellmewhen;
 
-/**
- * Created by tmcf on 14/03/2018.
- */
+import java.util.Date;
 
 public class Appointment {
+    private String title;
+    private String address;
+    private Date date;
+    private String notes;
+    private String time;
+
+
+    public Appointment(String title){this.title = title;}
+
+    public Appointment(String title, Date date, String time) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Appointment(String title, String address, Date date, String notes, String time) {
+        this.title = title;
+        this.address = address;
+        this.date = date;
+        this.notes = notes;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
 }
+
