e8be7029fa1b61e91998b097d657a9151c7dbb8a
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2018-03-14 14:48:25 +0200

    Implement AppointmentHandler basic functionality.
    Create basic functionality to simulate actual data used in the recycler
    view list.

--- app/src/main/java/fi/timomcfarlane/tellmewhen/AppointmentHandler.java
+++ app/src/main/java/fi/timomcfarlane/tellmewhen/AppointmentHandler.java
@@ -1,8 +1,25 @@
 package fi.timomcfarlane.tellmewhen;
 
-/**
- * Created by tmcf on 14/03/2018.
- */
+import android.content.Context;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AppointmentHandler {
+
+    private ArrayList<Appointment> apps;
+
+    private Context host;
+
+    public AppointmentHandler(Context host) {
+        this.host = host;
+        apps = new ArrayList<>();
+        apps.add(new Appointment("Test", Calendar.getInstance().getTime(), "12:00"));
+        apps.add(new Appointment("Test2", Calendar.getInstance().getTime(), "12:00"));
+        apps.add(new Appointment("Test3", Calendar.getInstance().getTime(), "12:00"));
+        apps.add(new Appointment("Test4", Calendar.getInstance().getTime(), "12:00"));
+    }
+
+    public ArrayList<Appointment> getAppointments() {
+        return this.apps;
+    }
 }
