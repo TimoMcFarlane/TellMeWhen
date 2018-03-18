4c61247f3f6f374e2fcb423a43e1702bb71bfd64
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2018-03-18 20:22:23 +0200

    Add CRUD functionality to AppointmentDao class

--- app/src/main/java/fi/timomcfarlane/tellmewhen/AppDatabase.java
+++ app/src/main/java/fi/timomcfarlane/tellmewhen/AppDatabase.java
@@ -1,7 +1,9 @@
 package fi.timomcfarlane.tellmewhen;
 
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+@Database(entities = {Appointment.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
-
+    public abstract AppointmentDao appointmentDao();
 }
--- app/src/main/java/fi/timomcfarlane/tellmewhen/AppointmentDao.java
+++ app/src/main/java/fi/timomcfarlane/tellmewhen/AppointmentDao.java
@@ -1,8 +1,25 @@
 package fi.timomcfarlane.tellmewhen;
 
-/**
- * Created by tmcf on 18/03/2018.
- */
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
+import java.util.List;
+
+@Dao
 public interface AppointmentDao {
+    @Query("SELECT * FROM appointment")
+    List<Appointment> getAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertAppointments(Appointment... appointments);
+
+    @Update
+    public void updateAppointments(Appointment... appointments);
+
+    @Delete
+    public void deleteAppointments(Appointment... appointments);
 }
