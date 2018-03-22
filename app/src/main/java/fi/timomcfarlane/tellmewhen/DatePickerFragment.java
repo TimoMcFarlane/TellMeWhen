b14d795b81c1e05c694cf7e7a2fee582921d56fb
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2018-03-22 16:03:29 +0200

    Implement DatePicker -fragment for Appointment Form
    User can set a date for the scheduled appointment

---
+++ app/src/main/java/fi/timomcfarlane/tellmewhen/DatePickerFragment.java
@@ -0,0 +1,36 @@
+package fi.timomcfarlane.tellmewhen;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+
+public class DatePickerFragment extends AppCompatDialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    private LocalBroadcastManager lBroadcast;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        lBroadcast = LocalBroadcastManager.getInstance(getContext());
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        Intent broadcast = new Intent("form_activity");
+        broadcast.putExtra("date", new int[] {i2,i1,i} );
+        lBroadcast.sendBroadcast(broadcast);
+    }
+}
